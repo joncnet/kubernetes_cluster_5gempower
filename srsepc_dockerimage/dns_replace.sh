5726b288ac4bbe7be8444218fac4fc6f708ca9b6
empower <g.baggio@create-net.org>
2019-02-22 10:57:28 +0100

    epc part of the commit

--- srsepc_dockerimage/dns_replace.sh
+++ srsepc_dockerimage/dns_replace.sh
@@ -1,4 +1,9 @@
-LOCAL_POD_ADDR=$(ip route get 1 | awk '{print $(NF-2);exit}')
+
+if [ "$local_pod_addr" ]; then
+    LOCAL_POD_ADDR=$local_pod_addr
+else
+    LOCAL_POD_ADDR=$(ip route get 1 | awk '{print $(NF-2);exit}')
+fi
 
 sed -i 's/LOCAL_REPLACE/'$LOCAL_POD_ADDR'/g' /srsLTE/build/srsepc/src/epc.conf
 
@@ -7,3 +12,7 @@ sed -i 's/SGI_IF_ADDR_REPLACE/'$sgi_if_addr'/g' /srsLTE/build/srsepc/src/epc.con
 sed -i 's/MME_CODE_REPLACE/'$mme_code'/g' /srsLTE/build/srsepc/src/epc.conf
 sed -i 's/MME_GROUP_REPLACE/'$mme_group'/g' /srsLTE/build/srsepc/src/epc.conf
 sed -i 's/TAC_REPLACE/'$tac'/g' /srsLTE/build/srsepc/src/epc.conf
+sed -i 's/MCC_REPLACE/'$mcc'/g' /srsLTE/build/srsepc/src/epc.conf
+sed -i 's/MNC_REPLACE/'$mnc'/g' /srsLTE/build/srsepc/src/epc.conf
+sed -i 's/DB_FILE_REPLACE/'$db_file'/g' /srsLTE/build/srsepc/src/epc.conf
+sed -i 's/DNS_ADDR_REPLACE/'$dns_addr'/g' /srsLTE/build/srsepc/src/epc.conf
--- srsepc_dockerimage/launcher.sh
+++ srsepc_dockerimage/launcher.sh
@@ -5,14 +5,28 @@ _term() {
   kill -TERM "$child"
 }
 
-trap myterm SIGTERM
+trap _term SIGTERM
 
 env
+
+if [ "$tunnelling" = "yes" ]; then
+    export local_ip=$(ip route get 1 | awk '{print $(NF-2);exit}')
+    ./tunnel_setup.py
+fi
+
 ./pod_ip_rest.sh &
 ./dns_replace.sh
 cat /srsLTE/build/srsepc/src/epc.conf
 cat /srsLTE/build/srsepc/src/mbms.conf
-./srsLTE/build/srsepc/src/srsepc_if_masq.sh `ip route get 1 | awk '{print $(NF-4);exit}'`
+cat /srsLTE/build/srsepc/src/$db_file.csv
+
+# temporary fix, user db file ignored by srslte
+cp /srsLTE/build/srsepc/src/$db_file.csv /root/.srs/user_db.csv 
+
+if [ -z "$local_pod_addr" -o "$tunnelling" = "yes" ]; then
+    ./srsLTE/build/srsepc/src/srsepc_if_masq.sh `ip route get 1 | awk '{print $(NF-4);exit}'`
+fi
+
 ./srsLTE/build/srsepc/src/srsepc /srsLTE/build/srsepc/src/epc.conf &
 
 child=$!
